f40e884c5b437963cba1ad1d376863c2d2996557
macao park <smu90@naver.com>
2023-06-18 19:05:15 +0900

    third commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const menu1 = () => {
+    document.getElementById("contentFrame").setAttribute("src","summary.html")
+}
+
+const menu2 = () => {
+    document.getElementById("contentFrame").setAttribute("src","summary2.html")
+}
+
+const menu3 = () => {
+    
+}
