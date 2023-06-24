8aee2bfb9a18486397436e41cc11ab3583f36e9a
macao park <smu90@naver.com>
2023-06-24 19:57:52 +0900

    forth commit

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 
 
-
-const bars = document.querySelector(".fas");
-const minimenu = document.querySelector("#minimenu");
-
-function onClick() {
-  minimenu.classList.toggle("hidden");
+// Function to toggle the bar
+function geeksforgeeks() {
+  let x = document.getElementById("menu_side");
+   
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
 }
 
-bars.addEventListener("click", onClick);
+// Function to toggle the plus menu into minus
+function myFunction(x) {
+  x.classList.toggle("hamberg");
+}
