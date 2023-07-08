3b1e9c1d89f9ffd0bbe1a032fe7e876d9d00b1cb
macao park <smu90@naver.com>
2023-07-08 14:17:59 +0900

    thrid commit

--- recommandation.js
+++ recommandation.js
@@ -1,35 +1,29 @@
-// function filterAnimals(e) {
-//     const animals = document.querySelectorAll(".list div"); // select all animal divs
-//     let filter = e.target.dataset.filter; // grab the value in the event target's data-filter attribute
-//     animals.forEach(animal => {
-//       animal.classList.contains(filter) // does the animal have the filter in its class list?
-//       ? animal.classList.remove('hidden') // if yes, make sure .hidden is not applied
-//       : animal.classList.add('hidden'); // if no, apply .hidden
-//     });
-//   };
 
 
-//   // code to add:
-// if (filter === '*') {
-//     animals.forEach(animal => animal.classList.remove('hidden'));
-//   }
-  
-//   // full JS code:
-//   function filterAnimals(e) {
-//     const animals = document.querySelectorAll(".list div");
-//     let filter = e.target.dataset.filter;
-//     if (filter === '*') {
-//       animals.forEach(animal => animal.classList.remove('hidden'));
-//     }  else {
-//       animals.forEach(animal => {
-//         animal.classList.contains(filter) ? 
-//         animal.classList.remove('hidden') : 
-//         animal.classList.add('hidden');
-//       });
-//     };
-//   };
+
+// 필터먹이기
+
+if (filter === '*') {
+  animals.forEach(animal => animal.classList.remove('hidden'));
+}
+
+function filterAnimals(e) {
+  const animals = document.querySelectorAll(".list div");
+  let filter = e.target.dataset.filter;
+  if (filter === '*') {
+    animals.forEach(animal => animal.classList.remove('hidden'));
+  }  else {
+    animals.forEach(animal => {
+      animal.classList.contains(filter) ? 
+      animal.classList.remove('hidden') : 
+      animal.classList.add('hidden');
+    });
+  };
+};
 
 
+
+// 햄버거 클릭하면 미니메뉴 나오기
 
 function show() {
   let x = document.getElementById("menu_mini");
