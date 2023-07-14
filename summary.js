89ae79f925564f76b2908f628beaa4d875bc5560
macao park <smu90@naver.com>
2023-07-14 20:09:36 +0900

    second commit

---
+++ summary.js
@@ -0,0 +1,18 @@
+// 햄버거 클릭하면 미니메뉴 나오기
+
+function show() {
+    let x = document.getElementById("menu_mini");
+     
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+  }
+
+
+  document.querySelector('#back').onclick = function() {
+    history.back();
+  }
+
+
